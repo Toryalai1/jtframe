503eeb502b8d4c04a617cf99a1e48ccd611260dd
Jose Tejada <jose.tejada@jotego.es>
2022-08-28 12:04:05 +0200

    jtcfgstr works

--- src/cmd/cfgstr.go
+++ src/cmd/cfgstr.go
@@ -5,36 +5,37 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/jotego/jtframe/jtcfgstr"
+	"github.com/jotego/jtframe/jtdef"
 	"github.com/spf13/cobra"
 )
 
+var cfg jtdef.Config
+
 // cfgstrCmd represents the cfgstr command
 var cfgstrCmd = &cobra.Command{
-	Use:   "cfgstr",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "cfgstr <core-name>",
+	Short: "Parse core variables",
+	Long: `Parses the jtcore-name.def file in the hdl folder and
+creates input files for simulation or synthesis`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cfgstr called")
+		cfg.Core = args[0]
+		jtcfgstr.Run(cfg, args)
 	},
+	Args: cobra.MinimumNArgs(1),
 }
 
 func init() {
 	rootCmd.AddCommand(cfgstrCmd)
+	flag := cfgstrCmd.PersistentFlags()
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cfgstrCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cfgstrCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	flag.StringVarP(&cfg.Target, "target", "t", "mist", "Target platform (mist, mister, sidi, neptuno, mc2, mcp, pocket)")
+	flag.StringVar(&cfg.Deffile, "parse", "", "Path to .def file")
+	flag.StringVar(&cfg.Template, "tpl", "", "Path to template file")
+	flag.StringVar(&cfg.Commit, "commit", "nocommit", "Commit ID")
+	flag.String("def", "", "Defines macro")
+	flag.String("undef", "", "Undefines macro")
+	flag.StringVar(&cfg.Output, "output", "cfgstr",
+		"Type of output: \n\tcfgstr -> config string\n\tbash -> bash script\n\tquartus -> quartus tcl\n\tsimulator name as specified in jtsim")
+	flag.BoolVar(&cfg.Verbose, "v", false, "verbose")
 }
--- src/cmd/root.go
+++ src/cmd/root.go
@@ -15,13 +15,11 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jtframe",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "File parser for JTFRAME projects. Jose Tejada (c) 2022",
+	Long: `File parser for JTFRAME projects. Jose Tejada (c) 2022
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Use jtframe to parse the core's def and yaml files to
+generate simulation and synthesis files`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -45,7 +43,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 
