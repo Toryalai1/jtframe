75cab31bd6e145ce407aad2298a6d822e8f07b6f
Jose Tejada <jotego@gmail.com>
2022-08-28 19:13:14 +0200

    mame2mra added

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module github.com/jotego/jtframe
 go 1.19
 
 require (
+	github.com/komkom/toml v0.1.2
 	github.com/spf13/cobra v1.5.0
 	gopkg.in/yaml.v2 v2.4.0
 )
---
+++ src/mra/dummy.go
@@ -0,0 +1,8 @@
+//go:build !pocket
+
+
+package mra
+
+func dump_pocket(machine *MachineXML, cfg Mame2MRA, args Args, macros map[string]string) {
+	// Does nothing
+}
